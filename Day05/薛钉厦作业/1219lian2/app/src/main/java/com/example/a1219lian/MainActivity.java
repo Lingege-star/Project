1e8c64616a7c6cbd2ceaf5fa4e6fb6dc4f5bcaa5
xuedidi <1119633919@qq.com>
2020-12-19 22:27:53 +0800

    xds作业

---
+++ Day05/薛钉厦作业/1219lian2/app/src/main/java/com/example/a1219lian/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.a1219lian;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+

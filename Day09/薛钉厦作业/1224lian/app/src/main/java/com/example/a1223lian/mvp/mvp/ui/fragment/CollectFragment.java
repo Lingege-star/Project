c1305b40a737572999b0b8fe740daa49a1541720
xuedidi <1119633919@qq.com>
2020-12-24 21:44:51 +0800

    xds作业

---
+++ Day09/薛钉厦作业/1224lian/app/src/main/java/com/example/a1223lian/mvp/mvp/ui/fragment/CollectFragment.java
@@ -0,0 +1,22 @@
+package com.example.a1223lian.mvp.mvp.ui.fragment;
+
+
+import com.example.a1223lian.mvp.base.BaseFragment;
+import com.example.a1223lian.mvp.base.BasePresenter;
+
+public class CollectFragment extends BaseFragment {
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+}
